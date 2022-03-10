3a07b18200e80a398a9eba22b367bfe837b4ad0e
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2022-03-10 09:03:54 -0300

    Atualizacao python

--- python/Desafio13.py
+++ python/Desafio13.py
@@ -1,5 +1,17 @@
-x=10;
-y=25;
-z=x+y;
+valorTotal = 0
 
-print ("sum of x+y =", z);
+qtdePessoas = int(input("Qtde Pessoas: "))
+numeroMesa = input("Numero Mesas: ")
+qtdeProdutos = int(input("Qtde Produtos: "))
+taxa = float(input("Taxa: "))
+
+for produto in range(qtdeProdutos):
+    qtde = int(input("Qtde: "))
+    valor = float(input("Valor: "))
+    valorTotal = valorTotal + (qtde * valor)
+
+valorTotal = valorTotal + (valorTotal * taxa / 100)
+
+valorIndividual = valorTotal / qtdePessoas
+
+print("Cada uma das",qtdePessoas,"pessoas da mesa",numeroMesa,"deve pagar R$",valorIndividual,"de conta")
